a2ef7c9f842c6e5582cc67f486bbaff1254fce54
Abdelrahman elyamany <abdelrahmantech1100@gmail.com>
2023-10-05 20:53:28 -0700

    200

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that print with print f function
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("which proper grammer, but the outcome is a piece of art,\n");
+	returen (0)
+}
