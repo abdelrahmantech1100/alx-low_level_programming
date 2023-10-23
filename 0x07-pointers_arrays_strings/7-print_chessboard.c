3e237917dc67e83305a5097f0e8bb2511f8a358f
Abdelrahman elyamany <abdelrahmantech1100@gmail.com>
2023-10-23 14:50:46 -0700

    n

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,13 +9,13 @@
 */
 void print_chessboard(char (*a)[8])
 {
-	int a;
+	int c;
 	int b;
 
-	for (a = 0; a < 8; a++)
+	for (c = 0; c < 8; c++)
 	{
 	for (b = 0; b < 8; b++)
-	_putchar(a[a][b]);
+	_putchar(c[c][b]);
 	_putchar('\n');
 	}
 }
