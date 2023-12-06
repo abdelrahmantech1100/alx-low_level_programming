86151f5e3b6efb7894562c2013e6a31250047c69
abdelrahmantech1100 <abdelrahmantech1100@gmail.com>
2023-12-06 03:24:41 -0800

    the_third_push

--- simple_shell/strings_functions.c
+++ simple_shell/strings_functions.c
@@ -1,86 +1,75 @@
 #include "shell.h"
 
 /**
- * _strcpy - copies a string
- * @dest: the destination
- * @src: the source
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: pointer to destionation
+ * Return: integer length of string
  */
-char *_strcpy(char *dest, char *src)
+int _strlen(char *s)
 {
 	int i = 0;
-	
-	if(dest == src || src == 0)
-	return(dest)
-	while(src[i])
-	{
-		dest[i] = src[i];
-		i++
-	}
-	dest[i] = 0;
-	return (dest);
-}
 
-/**
- * _strdup - duplicates a string
- * @str: the string to be duplicated
- *
- * Return: pointer to the duplicated striing
- */
-char *_strdup(const char *str)
-{
-	int length = 0;
-	char *ret;
+	if (!s)
+		return (0);
 
-	if (str == NULL)
-		return(NULL)
-	while (*str++)
-		length++;
-	ret = malloc(sizeof(char) * (length +1));
-	if(!ret)
-		return(NULL);
-	for(length++; length--)
-		ret[length] = *--str;
-	return (ret);
-}
-
-/**
- * _puts - print an input string
- * @str: the string to be printed
- *
- * Return: Nothing
- */
-void _puts(char *str)
-	int i = 0;
-
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
+	while (*s++)
 		i++;
-	}
+	return (i);
 }
 
 /**
- * _putchar - writes the characters to stdout
- * @c: the characters to be printed
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
  *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
-int _putchar(char c)
+int _strcmp(char *s1, char *s2)
 {
-	static int i;
-	static char buf[WROTE_BUFF_SIZE];
-
-	if (c == BUFF_FLUSH || i >= WRITE_BUFF_SIZE)
+	while (*s1 && *s2)
 	{
-		write(1, buf, i);
-		i = 0;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
+ *
+ * Return: address of next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
+ *
+ * Return: pointer to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
 }
